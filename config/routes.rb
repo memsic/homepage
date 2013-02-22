d4ffc1908b60b5312a8702555dc1f15dbb02ea7d
Brett Memsic <memsic@gmail.com>
2013-02-22 15:36:21 -0800

    Design updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Brett::Application.routes.draw do
+  root to: 'static_pages#home'
   
-  get "static_pages/home"
+  match '/home',    to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
